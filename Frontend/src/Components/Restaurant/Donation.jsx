a8ab8cfd7dc442272e0b389537546ea454070de7
Atharva Jamdade <168163867+atharvaj1425@users.noreply.github.com>
2025-04-04 11:20:48 +0530

    backticks

--- Frontend/src/Components/Restaurant/Donation.jsx
+++ Frontend/src/Components/Restaurant/Donation.jsx
@@ -21,7 +21,7 @@ const FoodDonationHistory = () => {
           return;
         }
 
-        const response = await axios.get('${import.meta.env.VITE_BASE_URL}/api/v1/restaurants/donationHistory', {
+        const response = await axios.get(`${import.meta.env.VITE_BASE_URL}/api/v1/restaurants/donationHistory`, {
           withCredentials: true,
         });
 
